32c810dbc9ff78ce4aa9fe7270fb596b8e2854c2
vitalii-ponomar <63642060+vitalii-ponomar@users.noreply.github.com>
2026-03-20 20:23:58 +1100

    Update and rename script.js to assets/script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-const toggle = document.querySelector('.nav-toggle');
-const nav = document.querySelector('.site-nav');
-if (toggle && nav) {
-  toggle.addEventListener('click', () => nav.classList.toggle('open'));
-}
-document.querySelectorAll('[data-year]').forEach(el => {
-  el.textContent = new Date().getFullYear();
-});
